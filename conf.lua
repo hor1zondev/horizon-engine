190f1dfd830b3df8153a58ecc86e5ef3887fd66a
hor1zondev <hor1zontaldev@gmail.com>
2026-09-16 11:23:42 +0300

    import json library and basic info.json file reading

--- conf.lua
+++ conf.lua
@@ -1,6 +1,22 @@
+local json = require "lib/json"
+
+EngineInfo = nil
 
 function love.conf(t)
-    -- TODO: make these configurable from game/info.json
-    t.window.width = 960; t.window.height = 540;
-    t.window.title = "game"
+    --Check if engine/info.json exists
+    if love.filesystem.getInfo("engine/info.json") == nil then
+        error("Could not find engine/info.json file in project directory.")
+    end
+    --Read engine/info.json and define it as a global variable
+    EngineInfo = json.decode(love.filesystem.read("engine/info.json"))
+    local gameInfoPath = EngineInfo.gameDirectory .. "/info.json"
+    --Check if gameDirectory/info.json exists
+    if love.filesystem.getInfo(gameInfoPath) == nil then
+        error("Could not find " .. gameInfoPath .. " file in project directory.")
+    end
+    --Having found the gameDirectory, read gameDirectory/info.json
+    GameInfo = json.decode(love.filesystem.read(gameInfoPath))
+    -- Set the config values from game info file
+    t.window.width = GameInfo.defaultWindowWidth; t.window.height = GameInfo.defaultWindowHeight;
+    t.window.title = GameInfo.windowTitle
 end
--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 
 function love.load()
-
+    
 end
 
 function love.update(delta)
@@ -8,5 +8,5 @@ function love.update(delta)
 end
 
 function love.draw()
-    love.graphics.print("hello world!")
+
 end
